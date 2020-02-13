236f79071f4afee7afcf09c48fe7ffea0f8a4a13
martin@fr3qu3ncy.co.uk <martinsa@it2ms.com>
2020-02-13 17:17:57 +0000

    update

--- pi-l-detect.py
+++ pi-l-detect.py
@@ -18,14 +18,45 @@
 # version  V1.0
 # date  2018-11-28
 
+# General imports
 import sys
 # sys.path.append('../')
 import time
-from DFRobot_AS3935_Lib import DFRobot_AS3935
-import RPi.GPIO as GPIO
 from datetime import datetime
 
-#I2C address
+# AS3935 imports
+from DFRobot_AS3935_Lib import DFRobot_AS3935
+import RPi.GPIO as GPIO
+
+# PiOLED imports
+import subprocess
+from board import SCL, SDA
+import busio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
+# PiOLED - initialise
+# Create the I2C interface.
+i2c = busio.I2C(SCL, SDA)
+# Create the SSD1306 OLED class.
+# The first two parameters are the pixel width and pixel height.  Change these
+# to the right size for your display!
+disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+# Clear display.
+disp.fill(0)
+disp.show()
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+# AS2925 - initialise
+# I2C address - AS3935
 AS3935_I2C_ADDR1 = 0X01
 AS3935_I2C_ADDR2 = 0X02
 AS3935_I2C_ADDR3 = 0X03
